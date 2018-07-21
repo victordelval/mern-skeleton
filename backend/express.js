204d602fd455339e3079da69dba0d9fbab48b96e
victordelval <victor.val.andres@gmail.com>
2018-07-21 21:28:05 +0200

    Add basic express server with template

---
+++ backend/express.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compress from 'compression'
+import cors from 'cors'
+import helmet from 'helmet'
+
+import Template from './../template'
+
+import userRoutes from './routes/user.routes'
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cookieParser())
+app.use(compress())
+app.use(helmet())
+app.use(cors())
+
+app.use('/', userRoutes)
+
+app.get('/', (req, res) => {
+  res.status(200).send(Template())
+})
+
+export default app
---
+++ template.js
@@ -0,0 +1,12 @@
+export default () => {
+  return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>MERN Kickstart</title>
+      </head>
+      <body>
+        <div id="root">Hello!</div>
+        </body>
+    </html>`
+}
