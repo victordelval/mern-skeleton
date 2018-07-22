ad84b84246fab9f94a71ba1bdbbeac4a8ce9802a
victordelval <victor.val.andres@gmail.com>
2018-07-22 20:51:11 +0200

    Update template font link

--- template.js
+++ template.js
@@ -3,12 +3,12 @@ export default () => {
     <html lang="en">
       <head>
         <meta charset="utf-8">
-        <title>MERN Kickstart</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+        <title>MERN App</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
       </head>
       <body>
-        <div id="root">Hello skeleton!</div>
+        <div id="root"></div>
         <script type="text/javascript" src="/dist/bundle.js"></script>
         </body>
     </html>`
