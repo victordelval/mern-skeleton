d34b9b52f09a74c0fdfdf8a93bef6cc9ca75d891
victordelval <victor.val.andres@gmail.com>
2018-07-22 20:52:26 +0200

    Add base frontend with App, Router and Home components

---
+++ frontend/core/Home.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+// import {withStyles} from '@material-ui/core/styles'
+import withStyles from "@material-ui/core/styles/withStyles";
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Typography from '@material-ui/core/Typography'
+import mountainsImg from './../assets/images/mountains.jpg'
+
+const styles = theme => ({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing.unit * 5
+  },
+  title: {
+    padding:`${theme.spacing.unit * 3}px ${theme.spacing.unit * 2.5}px ${theme.spacing.unit * 2}px`,
+    color: theme.palette.text.secondary
+  },
+  media: {
+    minHeight: 330
+  }
+})
+
+class Home extends Component {
+  render() {
+    const {classes} = this.props
+    return (
+        <Card className={classes.card}>
+          <Typography type="headline" component="h2" className={classes.title}>
+            Home Page
+          </Typography>
+          <CardMedia className={classes.media} image={mountainsImg} title="Unicorn Shells"/>
+          <CardContent>
+            <Typography type="body1" component="p">
+              Welcome to the Mountains...
+            </Typography>
+          </CardContent>
+        </Card>
+    )
+  }
+}
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+// using 'whitStyles' creates a HOC that has access
+// to the defined style objects as props
+export default withStyles(styles)(Home)
+
+
+// class Home extends Component {
+//   render() {
+//     const {classes} = this.props
+//     return (
+//         <p>Home</p>
+//     )
+//   }
+// }
+
+// export default withStyles(styles)(Home)
