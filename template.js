5bce2943c4bc0c4f0e3bd233f4da111c00026528
victordelval <victor.val.andres@gmail.com>
2018-07-23 18:13:26 +0200

    Improve template to accept markup and css

--- template.js
+++ template.js
@@ -1,20 +1,21 @@
-export default () => {
-  return `<!DOCTYPE html>
+export default ({ markup, css }) => {
+  return `<!doctype html>
     <html lang="en">
       <head>
         <meta charset="utf-8">
-        <title>MERN App</title>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
+        <title>MERN Skeleton</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         <style>
-          a {
-            text-decoration: none
-          }
+            a{
+              text-decoration: none
+            }
         </style>
       </head>
-      <body>
-        <div id="root"></div>
+      <body style="margin:0">
+        <div id="root">${markup}</div>
+        <style id="jss-server-side">${css}</style>
         <script type="text/javascript" src="/dist/bundle.js"></script>
-        </body>
+      </body>
     </html>`
 }
